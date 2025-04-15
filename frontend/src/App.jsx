46a879bf82a961766d7b1c010b7bd22eb30f37c9
singhpriyanhsu9415 <148875469+singhpriyanhsu9415@users.noreply.github.com>
2025-04-15 18:49:15 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Appointment from './pages/Appointment'
+import MyAppointments from './pages/MyAppointments'
+import MyProfile from './pages/MyProfile'
+import Footer from './components/Footer'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Verify from './pages/Verify'
+
+const App = () => {
+  return (
+    <div className='mx-4 sm:mx-[10%]'>
+      <ToastContainer />
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/doctors' element={<Doctors />} />
+        <Route path='/doctors/:speciality' element={<Doctors />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/appointment/:docId' element={<Appointment />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/verify' element={<Verify />} />
+      </Routes>
+      <button  onClick={() => window.location.href =import.meta.env.VITE_ADMIN_URL} className='flex bg-blue-500 items-center gap-2 text-white px-8 py-3 rounded-full text-[#595959] text-sm m-auto md:mx-100 hover:scale-105 transition-all duration-300'>Login as Admin</button>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
